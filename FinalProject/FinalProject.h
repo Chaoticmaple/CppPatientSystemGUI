26c8fe122583a978e9dd9bd50b445e4351ef3a64
Harry <brow1326@algonquinlive.com>
2022-08-12 15:44:29 -0400

    working

---
+++ FinalProject/FinalProject.h
@@ -0,0 +1,15 @@
+﻿// FinalProject.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <map>
+#include <nana/gui.hpp>
+#include <nana/gui/widgets/label.hpp>
+#include <nana/gui/widgets/button.hpp>
+#include <nana/gui/widgets/textbox.hpp>
+#include <nana/gui/widgets/listbox.hpp>
+#include <nana/gui/widgets/combox.hpp>
+
+// TODO: Reference additional headers your program requires here.
